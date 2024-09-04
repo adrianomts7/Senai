43cdd4b2fe7f771b5e496e1b42710597f1cd809d
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2024-09-04 11:18:05 +0000

    Atualização

--- POO/Python/Classes/Exercicio1.py
+++ POO/Python/Classes/Exercicio1.py
@@ -8,9 +8,13 @@ class Aluno:
     def __init__(self,nome: str,idade: int) -> None:
         self.nome = nome
         self.idade = idade
+    # Criando um método para exibir os dados.
+    def exibir_dados(self) -> str:
+        return (f"Nome: {self.nome}"
+            f"\nIdade: {self.idade}"
+            )
 
 #Instanciando classes.
 aluno = Aluno("Marta",22)
 
-print(aluno.nome)
-print(aluno.idade)
+print(aluno.exibir_dados())
