d44c55f50c7cfaacfca830b6edcfa7005cf27941
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2024-09-19 13:49:24 +0000

    Exercicio

---
+++ estruturadeprojetos/projeto_com_teste/models/pessoa.py
@@ -0,0 +1,21 @@
+from estruturadeprojetos.projeto_com_teste.models.enums.sexo import Sexo
+
+class Pessoa:
+   
+    def __init__(self,nome: str,idade: int,sexo: Sexo) -> None:
+        self._nome = nome 
+        self._idade = idade
+        self._sexo = sexo
+
+    def set_idade(self,idade):
+        if idade < 0:
+            self._idade
+        else:
+            self._idade = idade
+    
+    def __str__(self) -> str:
+        return (
+            f"Nome: {self._nome}"
+            f"\nIdade: {self._idade}"
+            f"\nSexo: {self._sexo.value}"
+        )
---
+++ estruturadeprojetos/projeto_com_teste/tests/test pessoa.py
@@ -0,0 +1,22 @@
+import pytest
+from estruturadeprojetos.projeto_com_teste.models.enums.sexo import Sexo
+from estruturadeprojetos.projeto_com_teste.models.pessoa import Pessoa
+
+@pytest.fixture
+def pessoa_valida():
+    pessoa = Pessoa("Marta",22,Sexo.FEMININO)
+    return pessoa
+
+def test_pessoa_mudar_nome(pessoa_valida):
+    pessoa_valida._nome = "Jose"
+    assert pessoa_valida._nome == "Jose"
+
+def test_pessoa_nome_valido(pessoa_valida):
+    assert pessoa_valida._nome == "Marta"
+
+def test_pessoa_idade_valida(pessoa_valida):
+    assert pessoa_valida._idade == 22 
+
+def test_pessoa_idade_negativa(pessoa_valida):
+    pessoa_valida.set_idade(-1)
+    assert pessoa_valida._idade == 0
