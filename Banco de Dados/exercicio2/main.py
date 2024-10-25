dfb9e0f3548f8c5ca6ff468f40df23b15633fab8
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2024-10-25 20:35:25 +0000

    Funcionalidade extras adicionadas

--- Banco de Dados/exercicio2/main.py
+++ Banco de Dados/exercicio2/main.py
@@ -1,16 +1,23 @@
 from servicos.aluno_servico import AlunosServices
 from repositorio.aluno_repositorio import AlunoRepository
 from configuracao.conexao import Session
+import os
 
 session = Session()
 repositorio = AlunoRepository(session)
 services = AlunosServices(repositorio)
 
-ra = str(input("Digite o seu ra: "))
-nome = str(input("Digite o seu nome: "))
-sobrenome = str(input("Digite o seu sobrenome: "))
-email = str(input("Digite o seu e-mail: "))
-senha = str(input("Digite a sua senha: "))
+inscricao = int(input("Deseja inscrever quantos alunos: "))
+
+os.system('cls || clear')
+
+for i in range(inscricao):
+    ra = str(input("Digite o seu ra: "))
+    nome = str(input("Digite o seu nome: "))
+    sobrenome = str(input("Digite o seu sobrenome: "))
+    email = str(input("Digite o seu e-mail: "))
+    senha = str(input("Digite a sua senha: "))
+
 
 services.criar_aluno(ra=ra, nome=nome, sobrenome=sobrenome, email=email, senha=senha)
 
@@ -18,5 +25,4 @@ alunos = services.listar_todos_alunos()
 
 for aluno in alunos:
     print(f"{aluno.ra} - {aluno.nome} - {aluno.sobrenome}")
-
 
