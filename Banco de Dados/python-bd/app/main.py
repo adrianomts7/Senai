04a587a3a7340e012cca8cdeed253b89fcdf246c
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2024-11-03 10:16:21 -0300

    Add files via upload

---
+++ Banco de Dados/python-bd/app/main.py
@@ -0,0 +1,81 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.databases import Session
+import os
+
+def main():
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    while True:
+        print("\n === SENAI SOLUTION ===")
+        print(" 1 - Adiconar Usuario ")
+        print(" 2 - Pesquisar um usuario")
+        print(" 3 - Atualizar dados de um usuario")
+        print(" 4 - Excluir um usuario")
+        print(" 5 - Exibir todos os usuarios cadastrados")
+        print(" 0 - Sair")
+        
+        try:
+            opcao = int(input("Digite a opção que deseja: "))
+        except:
+            print("Digite somente números inteiros.")
+            continue
+        
+        match opcao:
+            case 1:
+                os.system("cls || clear")
+                nome = input("Digite o nome do usuario: ")
+                email = input("Digite o email do usuario: ")
+                senha = input("Digite o senha do usuario: ")
+                
+                service.criar_usuario(nome=nome, email=email, senha=senha)  
+                
+                continue
+            
+            case 2:
+                os.system("cls || clear")
+                
+                email = input("Digite o email do usuario que deseja procurar: ")
+                service.procurar_usuario(email=email)
+                
+                continue
+                
+            case 3:
+                os.system("clear || cls")
+                
+                emailUsuario = input("Digite o email do usuario que irá ser atualizado: ")
+                nome = input("Nome atualizado do usuario: ")
+                email = input("Digite o email atualizado do usuario: ")
+                senha = input("Digite a senha atualizada do usuario: ")
+                
+                service.atualizar_usuario(email=emailUsuario,nomeAtualizado=nome, emailAtualizado=email, senhaAtualizada=senha)
+                
+                continue
+            
+            case 4:
+                os.system("clear || cls")
+                
+                email = input("Digite o email do usuario que vai apagar: ")
+                service.excluir_usuario(email=email)
+                
+                continue
+            
+            case 5:
+                os.system("cls || clear")
+                
+                lista_usuarios = service.listar_todos_usuarios()
+                for usuario in lista_usuarios:
+                    print(f"\nNome: {usuario.nome}  \nemail: {usuario.email}  \nsenha: {usuario.senha}")
+                
+                continue
+            
+            case 0:
+                os.system("cls || clear")
+                print("Finalizando sistema")
+                break
+
+if __name__ == "__main__":
+    os.system("cls || clear")
+    main()
---
+++ Banco de Dados/python-bd/app/services/usuario_service.py
@@ -0,0 +1,73 @@
+from models.usuario_models import Usuario
+from repositories.usuario_repository import UsuarioRepository
+
+class UsuarioService:
+    def __init__(self, repository: UsuarioRepository) -> None:
+        self.repository = repository
+
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try:
+            usuario = Usuario(nome=nome,email=email,senha=senha)
+
+            cadastrado = self.repository.pesquisar_usuario_por_email(usuario.email)
+
+            if cadastrado:
+                print("Usuario já cadastrado!")
+                return
+
+            self.repository.salvar_usuario(usuario=usuario)
+            print("Usuario cadastrado com sucesso")
+
+        except TypeError as erro:
+            print(f"Erro ao salvar o usuario: {erro}")
+
+        except Exception as erro:
+            print (f"Ocorreu um erro inesperado: {erro}")
+
+    def listar_todos_usuarios(self):
+        return self.repository.listar_usuarios()    
+        
+    def procurar_usuario(self,email: str):
+        try:
+            cadastrado = self.repository.pesquisar_usuario_por_email(email=email)
+
+            if cadastrado:
+                print("Usuario Encontrado com sucesso")
+                print(f"{cadastrado.nome} - {cadastrado.email}")
+        
+        except TypeError as erro:
+            print(f"Erro ao procurar usuario: {erro}")
+            
+        except Exception as erro:
+            print(f"Erro inesperado: {erro}")
+            
+    def atualizar_usuario(self,email: str,nomeAtualizado: str, emailAtualizado: str, senhaAtualizada: str):
+        try:
+            usuario = self.repository.pesquisar_usuario_por_email(email=email)
+                
+            if usuario:
+                usuario.nome = nomeAtualizado
+                usuario.email = emailAtualizado
+                usuario.senha = senhaAtualizada
+                self.repository.session.commit()
+                print("Usuario Atualizado com sucesso!")
+                
+        except TypeError as erro:
+            print(f"Erro ao tentar atualizar usuario: {erro}")
+            
+    def excluir_usuario(self,email: str):
+        try:
+            cadastrado = self.repository.pesquisar_usuario_por_email(email=email)
+            
+            if not cadastrado:
+                print("Usuario não encontrado")
+                return    
+            
+            self.repository.excluir_usuario(usuario=cadastrado)
+            print("Usuario Excluido com sucesso!")
+            
+        except TypeError as erro:
+            print(f"Erro ao apagar usuario: {erro}")
+            
+        except Exception as erro:
+            print(f"Ocorreu um erro inesperado: {erro}")   
