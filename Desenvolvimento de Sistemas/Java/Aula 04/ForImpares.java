a3fadcdb08943fb507237a978fe7235811afa015
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2025-02-13 09:29:56 -0300

    Novos Exercicios

---
+++ Desenvolvimento de Sistemas/Java/Aula 04/ForImpares.java
@@ -0,0 +1,12 @@
+public class ForImpares {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 51; i+= 2){
+            System.out.println(i);
+        }
+
+        for(int j = 52; j <= 100; j += 2){
+            System.out.println(j);
+        }
+    }
+    
+}
---
+++ Desenvolvimento de Sistemas/Java/Aula 04/TabuadaNum.java
@@ -0,0 +1,10 @@
+public class TabuadaNum {
+    public static void main(String[] args) {
+        int num = 5;
+        
+        for(int i = 1; i <= 10; i++){
+
+            System.out.println(num + " X " + i + " = " + num * i);
+        }
+    }
+}
