4f6c40d19765cc14f5ed84b19ffe1b02e2507dee
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2025-02-27 18:46:10 -0300

    Resto dos exercicios, da tarefas

---
+++ Desenvolvimento de Sistemas/Java/Tarefa.java
@@ -0,0 +1,28 @@
+public class Tarefa {
+    private String descricao;
+    private String status;
+    
+    public Tarefa(String descricao, String status) {
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+
+}
