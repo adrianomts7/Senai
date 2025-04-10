9fb05b35f35f44e4863b2d6293218a33e5293f16
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2025-04-10 10:55:37 -0300

    Fazendo telas com java

---
+++ Desenvolvimento de Sistemas/Java/Aula 14/soma/Soma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soma;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Soma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
