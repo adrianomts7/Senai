89f76f049982c526e6bcaf5b339e38e06c2aa539
Adriano Mateus <161032614+adrianomts7@users.noreply.github.com>
2025-04-14 11:33:13 -0300

    Exercicio Incompleto

---
+++ Desenvolvimento de Sistemas/Java/Aula 16/javinha/Usuario.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javinha;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Usuario {
+    private String usuario;
+    private String senha;
+
+    public Usuario (String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
